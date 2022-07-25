18a4faa24be70bad8e17c1d6b5bab764b72b12ac
Scott Wysocki <swysocki@gmail.com>
2022-07-25 08:52:50 -0400

    fix: add disk image data to string repr

--- tests/test_disk.py
+++ tests/test_disk.py
@@ -6,15 +6,14 @@ from gpt_image.disk import Disk
 from gpt_image.partition import Partition
 
 BYTE_DATA = b"\x01\x02\x03\x04"
-DISK_SIZE = 2 * 1024 * 1024
-OFFSET = 32
+DISK_SIZE = 4 * 1024 * 1024  # 4 MB
 
 
 @pytest.fixture
 def new_image(tmp_path):
     image_name = tmp_path / "test.img"
     disk = Disk(image_name)
-    disk.create(DISK_SIZE * 2)
+    disk.create(DISK_SIZE)
     part1 = Partition("partition1", 2 * 1024, Partition.LINUX_FILE_SYSTEM)
     part2 = Partition("partition2", 3 * 1024, Partition.LINUX_FILE_SYSTEM)
     disk.table.partitions.add(part1)
@@ -35,7 +34,7 @@ def test_disk_create(tmp_path):
 def test_disk_open(new_image):
     disk = Disk(new_image)
     disk.open()
-    assert disk.size == DISK_SIZE * 2
+    assert disk.size == DISK_SIZE
     assert disk.table.primary_header.backup is False
     assert disk.table.secondary_header.backup is True
     assert disk.table.primary_header.partition_entry_lba == 2
@@ -49,6 +48,8 @@ def test_disk_info(new_image):
     disk.open()
     disk_s = str(disk)
     disk_d = json.loads(disk_s)
+    assert disk_d["path"] == str(new_image)
+    assert disk_d["image_size"] == DISK_SIZE
     assert not disk_d["primary_header"]["backup"]
     assert disk_d["backup_header"]["backup"]
     assert disk_d["backup_header"]["header_size"] == 92
