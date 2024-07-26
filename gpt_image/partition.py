f356c341ca506179224c20584eb456bd400d1aec
Woodrow Barlow <woodrow.barlow@analog.com>
2024-07-26 15:31:20 -0400

    feat: support for deleting/resizing existing partitions

--- gpt_image/partition.py
+++ gpt_image/partition.py
@@ -1,11 +1,13 @@
 from __future__ import annotations
 
 import json
+import shutil
 import struct
+import tempfile
 import uuid
 from enum import Enum, IntEnum
 from math import ceil
-from typing import List, Optional
+from typing import List, Optional, Any, IO
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:  # a bit of a hack to allow typing to work
@@ -82,6 +84,33 @@ class PartitionType(Enum):
     STORAGE_REPLICA_PARTITION = "558D43C5-A1AC-43C0-AAC8-D1472B2923D1"
 
 
+class StagedAttribute:
+
+    def __init__(self, value: Any):
+        self._staged = value
+        self._value = value
+
+    @property
+    def value(self) -> Any:
+        return self._value
+
+    @value.setter
+    def value(self, value: Any) -> None:
+        self._staged = value
+
+    @property
+    def staged_value(self) -> Any:
+        return self._staged
+
+    def needs_commit(self) -> bool:
+        if self._value == self._staged:
+            return False
+        return True
+
+    def commit(self) -> None:
+        self._value = self._staged
+
+
 class Partition:
     """Partition class represents a GPT partition
 
@@ -118,16 +147,53 @@ class Partition:
         # if the partition GUID is empty, generate one
         if not partition_guid:
             self.partition_guid = str(uuid.uuid4())
-        self.first_lba = 0
-        self.last_lba = 0
+        self._first_lba = StagedAttribute(0)
+        self._last_lba = StagedAttribute(0)
         self._attribute_flags = partition_attributes
         self.alignment = alignment
-        self.size = size
+        self._size = StagedAttribute(0)
+        self._size.value = size
 
     def __repr__(self) -> str:
         partitionvalue = {k: v for k, v in vars(self).items() if not k.startswith("_")}
         partitionvalue["attribute_flags"] = self.attribute_flags
         return json.dumps(partitionvalue, indent=2, ensure_ascii=False)
+
+    @property
+    def first_lba(self) -> int:
+        return int(self._first_lba.value)
+
+    @first_lba.setter
+    def first_lba(self, value: int) -> None:
+        self._first_lba.value = value
+
+    @property
+    def first_lba_staged(self) -> int:
+        return int(self._first_lba.staged_value)
+
+    @property
+    def last_lba(self) -> int:
+        return int(self._last_lba.value)
+
+    @last_lba.setter
+    def last_lba(self, value: int) -> None:
+        self._last_lba.value = value
+
+    @property
+    def last_lba_staged(self) -> int:
+        return int(self._last_lba.staged_value)
+
+    @property
+    def size(self) -> int:
+        return int(self._size.value)
+
+    @size.setter
+    def size(self, value: int) -> None:
+        self._size.value = value
+
+    @property
+    def size_staged(self) -> int:
+        return int(self._size.staged_value)
 
     @property
     def attribute_flags(self) -> List[int]:
@@ -175,45 +241,98 @@ class Partition:
         partition_bytes = self._PARTITION_FORMAT.pack(
             uuid.UUID(self.type_guid).bytes_le,
             uuid.UUID(self.partition_guid).bytes_le,
-            self.first_lba,
-            self.last_lba,
+            self.first_lba_staged,
+            self.last_lba_staged,
             attributes_value,
             bytes(self.partition_name, encoding="utf_16_le"),
         )
         return partition_bytes
 
-    def write_data(self, disk: Disk, data: bytes) -> int:
+    def _staged_attrs(self) -> List[StagedAttribute]:
+        return [attr
+                for attr in [self._first_lba, self._last_lba, self._size]
+                if attr.needs_commit()]
+
+    def _commit_attrs(self) -> None:
+        for attr in self._staged_attrs():
+            attr.commit()
+
+    def needs_commit(self) -> bool:
+        if len(self._staged_attrs()) == 0:
+            return False
+        return True
+
+    def commit(self, disk: Disk, tmpfile: Optional[IO[bytes]] = None) -> None:
+        if tmpfile is None:
+            f: IO[bytes] = open(disk.image_path, 'r+b')
+        else:
+            f = tmpfile
+        try:
+            max_size = min(self.size, self.size_staged)
+            data = self.read(disk, max_size=max_size)
+            start = disk.sector_size * self.first_lba_staged
+            self._write_data(f, start, bytes(data))
+            self._commit_attrs()
+        finally:
+            if tmpfile is None:
+                f.close()
+
+    def _write_data(self, image: IO[bytes], start_offset: int, data: bytes) -> int:
+        image.seek(start_offset)
+        image.write(data)
+        return len(data)
+
+    def write_data(self, disk: Disk, data: bytes, offset: int = 0) -> int:
         """Write bytes to partition
 
         Args:
             disk: GPT Disk instance
             data: data in bytes
+            offset: an offset (number of bytes) within the partition at which to write
         Returns:
             integer of byte count written
         Raises:
             ValueError if byte count is too large for partition
         """
-        if len(data) > self.size:
-            raise ValueError(f"data too large for partition: {len(data)} {self.size}")
-        with open(str(disk.image_path), "r+b") as b:
-            start = self.first_lba * disk.sector_size
-            b.seek(start)
-            b.write(data)
-        return len(data)
 
-    def read(self, disk: Disk) -> bytearray:
+        if len(data) + offset > self.size:
+            raise ValueError(f"data too large for partition: {len(data)} + {offset} > {self.size}")
+        start = disk.sector_size * self.first_lba + offset
+        with open(disk.image_path, 'r+b') as image:
+            return self._write_data(image, start, data)
+
+    def read(self, disk: Disk, max_size: Optional[int] = None) -> bytearray:
         """Read bytes from a given partition
 
         Args:
             disk: GPT Disk instance
+            max_size: a maximum number of bytes to read (or None to read the entire partition)
         Returns:
             bytearray of partition data
         """
+
         with open(str(disk.image_path), "rb") as b:
-            start = self.first_lba * disk.sector_size
+            start = disk.sector_size * self.first_lba
+            size = self.size
+            if max_size is not None:
+                size = min(size, max_size)
             b.seek(start)
-            buffer = b.read(self.size)
+            buffer = b.read(size)
         return bytearray(buffer)
+
+    def matches_name_or_guid(self, name_or_guid: str) -> bool:
+        """Checks whether this partition matches the provided string. This can match
+        on partition name (case-sensitive) or partition GUID (case-insensitive).
+
+        Returns:
+            True if the string matches this partition, False otherwise.
+        """
+
+        if self.partition_name == name_or_guid:
+            return True
+        if self.partition_guid.lower() == name_or_guid.lower():
+            return True
+        return False
 
     @staticmethod
     def unmarshal(partition_bytes: bytes, sector_size: int) -> "Partition":
@@ -247,6 +366,7 @@ class Partition:
         )
         part.first_lba = first_lba
         part.last_lba = last_lba
+        part._commit_attrs()
         return part
 
 
@@ -277,17 +397,85 @@ class PartitionEntryArray:
                 boundaries
         """
 
-        partition.first_lba = self._get_first_lba(partition)
+        partition.first_lba = self._get_first_lba(partition, self.entries)
         partition.last_lba = self._get_last_lba(partition)
 
-        if partition.last_lba > self._geometry.last_usable_lba:
+        if partition.last_lba_staged > self._geometry.last_usable_lba:
             raise PartitionEntryError(
                 "partition overflows the last allowed Logical Block Address: "
-                f"{self._geometry.last_usable_lba} requested: {partition.last_lba}"
+                f"{self._geometry.last_usable_lba} requested: {partition.last_lba_staged}"
             )
         self.entries.append(partition)
 
-    def _get_first_lba(self, partition: Partition) -> int:
+    def resize(self, partition_name_or_guid: str, size: int) -> Partition:
+        """Resize a partition in place. This may truncate data.
+
+        Args:
+            partition_name_or_guid: string name of partition to resize
+            size: new integer size of partition in bytes
+        Returns:
+            the partition instance
+        Raises:
+            NameError if the partition was not found
+        """
+
+        entries: List[Partition] = []
+        matched_partition: Optional[Partition] = None
+        for partition in self.entries:
+            if partition.matches_name_or_guid(partition_name_or_guid):
+                partition.size = size
+                matched_partition = partition
+            if matched_partition is not None:
+                partition.first_lba = self._get_first_lba(partition, entries)
+                partition.last_lba = self._get_last_lba(partition)
+            entries.append(partition)
+        if matched_partition is None:
+            raise NameError(partition_name_or_guid)
+        self.entries = entries
+        return matched_partition
+
+    def remove(self, partition_name_or_guid: str) -> Partition:
+        """Remove a partition from the list of entries
+
+        Args:
+            partition_name_or_guid: string name of partition to remove
+        Returns:
+            the partition instance
+        Raises:
+            NameError if the partition was not found
+        """
+
+        entries: List[Partition] = []
+        matched_partition: Optional[Partition] = None
+        for partition in self.entries:
+            if partition.matches_name_or_guid(partition_name_or_guid):
+                matched_partition = partition
+                continue
+            if matched_partition is not None:
+                partition.first_lba = self._get_first_lba(partition, entries)
+                partition.last_lba = self._get_last_lba(partition)
+            entries.append(partition)
+        if matched_partition is None:
+            raise NameError(partition_name_or_guid)
+        self.entries = entries
+        return matched_partition
+
+    def commit(self, disk: Disk) -> None:
+        """Shift partition data within the disk based on any staged LBA/size modifications,
+        then commit all staged LBA/size modifications.
+
+        Args:
+            disk: GPT Disk instance
+        """
+
+        if not any([partition.needs_commit() for partition in self.entries]):
+            return
+        with tempfile.NamedTemporaryFile() as tmpfile:
+            for partition in self.entries:
+                partition.commit(disk, tmpfile)
+            shutil.copy(tmpfile.name, disk.image_path)
+
+    def _get_first_lba(self, partition: Partition, entries: List[Partition]) -> int:
         """Calculate the first LBA of a new partition
 
         Search for the largest LBA, this will be used to calculate the first
@@ -298,6 +486,7 @@ class PartitionEntryArray:
 
         Args:
             partition: instance of the Partition class to calculate LBA for
+            entries: list of Partition instances in which LBA is already calculated
         Returns:
             integer of the first LBA
         """
@@ -307,8 +496,8 @@ class PartitionEntryArray:
             return (m + 1) * alignment
 
         largest_lba = 0
-        for part in self.entries:
-            lba = part.last_lba
+        for part in entries:
+            lba = part.last_lba_staged
             if int(lba) > int(largest_lba):
                 largest_lba = lba
         last_lba = 33 if largest_lba == 0 else largest_lba
@@ -327,12 +516,12 @@ class PartitionEntryArray:
             PartitionEntryError if the target partition smaller than a sector
         """
 
-        if partition.size < self._geometry.sector_size:
+        if partition.size_staged < self._geometry.sector_size:
             raise PartitionEntryError("Partition smaller than sector size")
 
         # round the LBA up to ensure our LBA will hold the partition
-        lba = int(ceil(partition.size / self._geometry.sector_size))
-        f_lba = int(partition.first_lba)
+        lba = int(ceil(partition.size_staged / self._geometry.sector_size))
+        f_lba = int(partition.first_lba_staged)
         return (f_lba + lba) - 1
 
     def marshal(self) -> bytes:
@@ -351,15 +540,16 @@ class PartitionEntryArray:
         )
         return padded
 
-    def find(self, partition_name: str) -> Optional[Partition]:
-        """Find a Partition by name
+    def find(self, partition_name_or_guid: str) -> Optional[Partition]:
+        """Find a Partition by name or GUID
 
         Args:
-            partition_name: string name of partition to search for
+            partition_name_or_guid: string name (or string GUID) of partition to search for
         Returns:
             the partition instance or None if not found
         """
+
         for partition in self.entries:
-            if partition.partition_name == partition_name:
+            if partition.matches_name_or_guid(partition_name_or_guid):
                 return partition
         return None
